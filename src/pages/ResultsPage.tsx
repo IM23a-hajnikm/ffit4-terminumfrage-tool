2a4bdda4a3fc0378e51daab6916b5bef0e81b62a
Martin Hajnik <145665552+IM23a-hajnikm@users.noreply.github.com>
2026-03-24 10:57:32 +0100

    Issue 5.1 LU05.A01: fix CI setup-node lockfile error in frontend workflow

---
+++ src/pages/ResultsPage.tsx
@@ -0,0 +1,34 @@
+import { Link, useParams } from 'react-router-dom';
+import { VoteTable } from '../components/VoteTable';
+import { PollNotFound } from '../components/PollNotFound';
+import { getPoll } from '../utils/storage';
+
+export function ResultsPage() {
+  const { pollId = '' } = useParams();
+  const poll = getPoll(pollId);
+
+  if (!poll) {
+    return <PollNotFound />;
+  }
+
+  return (
+    <section className="stack">
+      <div className="card">
+        <h1>{poll.title} – Resultate</h1>
+        {poll.description && <p>{poll.description}</p>}
+        <div className="actions">
+          <Link to={`/poll/${poll.id}`} className="button-link">
+            Zur Abstimmung
+          </Link>
+          <Link to="/create" className="button-link secondary-link">
+            Neue Umfrage erstellen
+          </Link>
+        </div>
+      </div>
+
+      <div className="card">
+        <VoteTable poll={poll} />
+      </div>
+    </section>
+  );
+}
